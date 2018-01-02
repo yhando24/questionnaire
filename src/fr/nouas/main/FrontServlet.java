12e30656b504bf045742a620e6a478d86de7c217
yhando24 <youcef.handoura@gmail.com>
2018-01-02 14:50:44 +0100

    noas 0.1.6

--- src/fr/nouas/main/FrontServlet.java
+++ src/fr/nouas/main/FrontServlet.java
@@ -2,8 +2,7 @@ package fr.nouas.main;
 
 import java.io.IOException;
 
-<<<<<<< HEAD
-=======
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
->>>>>>> 1be068c5097978b8d41ec8efbf2ffa154b1c4fba
 @WebServlet("/FrontServlet")
 public class FrontServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
