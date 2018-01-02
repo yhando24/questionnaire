f1cf8375e971ce5a9bb6d3537e4be7b2e07e48e7
lokm <vincent.olivan@gmail.com>
2018-01-02 10:58:52 +0100

    nouas 0.1.1

---
+++ src/fr/nouas/main/FrontServlet.java
@@ -0,0 +1,24 @@
+package fr.nouas.main;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontServlet")
+public class FrontServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public FrontServlet() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
