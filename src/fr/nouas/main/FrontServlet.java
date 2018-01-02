1afb4ebe8d11824a102e9ea44c1bba50f4885a82
yhando24 <youcef.handoura@gmail.com>
2018-01-02 12:16:38 +0100

    nouas 0.1.5

--- src/fr/nouas/main/FrontServlet.java
+++ src/fr/nouas/main/FrontServlet.java
@@ -2,11 +2,7 @@ package fr.nouas.main;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/FrontServlet")
 public class FrontServlet extends HttpServlet {
