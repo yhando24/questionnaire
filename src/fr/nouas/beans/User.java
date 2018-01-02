1726030a45c7b1ca61f71c78b8e43d2f218888a6
lokm <vincent.olivan@gmail.com>
2018-01-02 11:53:24 +0100

    nouas 0.1.1

--- src/fr/nouas/beans/User.java
+++ src/fr/nouas/beans/User.java
@@ -1,5 +1,6 @@
 package fr.nouas.beans;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,6 +11,80 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(length=70, nullable=false)
+	private String nom;
+	@Column(length=70, nullable=false)
+	private String prenom;
+	@Column(length=90, nullable=false, unique=true)
+	private String email;
+	@Column(length=40, nullable=false)
+	private String password;
+	@Column(length=20, nullable=false)
+	private String role;
+	
+	public User() {}
+	
+	public User(String nom, String prenom, String email, String password, String role) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public User(int id, String nom, String prenom, String email, String password, String role) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 	
 	
 }
