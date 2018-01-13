88d6e614e2121c8ee06a3bab08734a84d6a58e63
lokm <vincent.olivan@gmail.com>
2018-01-13 03:00:25 +0100

    export pdf Ok

--- WebContent/WEB-INF/questionnaire.jsp
+++ WebContent/WEB-INF/questionnaire.jsp
@@ -10,6 +10,7 @@
 	type="text/javascript"></script>
 <script src='<c:url value="/resources/js/ajax.js" />'
 	type="text/javascript"></script>
+	<script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js"></script>
 <title>Questionnaire</title>
 </head>
@@ -116,31 +117,12 @@
 				<input type="hidden" name="category" form="createPdf" value="${questionnaire.category.id}">
 				<input type="hidden" name="questionnaire" form="createPdf" value="${questionnaire.id}">
 				<input type="hidden" name="user" form="createPdf" value="${user.id}">
-				<input type="submit" class="controls" value="Exporter" form="createPdf" />
+				<input type="submit" class="controls" value="Exporter" />
 		</article>
 
-<div id="editor"></div>
-<script type="text/javascript">
-var doc = new jsPDF();
-
-//We'll make our own renderer to skip this editor
-var specialElementHandlers = {
-	'#editor': function(element, renderer){
-		return true;
-	},
-	'.controls': function(element, renderer){
-		return true;
-	}
-};
-
-//All units are in the set measurement for the document
-//This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-doc.fromHTML($('body').get(0), 15, 15, {
-	'width': 170, 
-	'elementHandlers': specialElementHandlers
-});
-doc.save('test.pdf');
-</script>
+<img id="screenshot-img" src="" alt="" />
+<script src='<c:url value="/resources/js/createPdf.js" />'
+	type="text/javascript"></script>
 	</section>
 </body>
 </html>
---
+++ WebContent/resources/js/createPdf.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+	$( window ).on( "load", function() {
+
+	$(document).on("click",".controls",function() {
+			
+		
+		var makeHighResScreenshot = function(srcEl, destIMG, dpi) {
+		    var scaleFactor = 1;
+		    // Save original size of element
+		    var originalWidth = srcEl.offsetWidth;
+		    var originalHeight = srcEl.offsetHeight;
+		    // Save original document size
+		    var originalBodyWidth = document.body.offsetWidth;
+		    var originalBodyHeight = document.body.offsetHeight;
+
+		    // Add style: transform: scale() to srcEl
+		    srcEl.style.transform = "scale(" + scaleFactor + ", " + scaleFactor + ")";
+		    srcEl.style.transformOrigin = "left top";
+
+		    // create wrapper for srcEl to add hardcoded height/width
+		    var srcElWrapper = document.createElement('div');
+		    srcElWrapper.id = srcEl.id + '-wrapper';
+		    srcElWrapper.style.height = originalHeight*scaleFactor + 'px';
+		    srcElWrapper.style.width = originalWidth*scaleFactor + 'px';
+		    // insert wrapper before srcEl in the DOM tree
+		    srcEl.parentNode.insertBefore(srcElWrapper, srcEl);
+		    // move srcEl into wrapper
+		    srcElWrapper.appendChild(srcEl);
+
+		    // Temporarily remove height/width constraints as necessary
+		    document.body.style.width = originalBodyWidth*scaleFactor +"px";
+		    document.body.style.height = originalBodyHeight*scaleFactor +"px";
+
+		    window.scrollTo(0, 0); // html2canvas breaks when we're not at the top of the doc, see html2canvas#820
+		    html2canvas(srcElWrapper, {
+		        onrendered: function(canvas) {
+		            destIMG.src = canvas.toDataURL("image/png");
+		            srcElWrapper.style.display = "none";
+		            var doc = new jsPDF('p', 'mm', [canvas.height, canvas.width]);
+//					   var width = doc.internal.pageSize.width;    
+//					   var height = doc.internal.pageSize.height;
+					   
+					   doc.addImage(img,'JPEG', 0, 0, canvas.width, canvas.height);
+					   doc.save('test.pdf');
+		            // Reset height/width constraints
+		            document.body.style.width = originalBodyWidth + "px";
+		            document.body.style.height = originalBodyHeight + "px";
+		            
+		        }
+		    });
+		};
+		var src = document.getElementById('questionnaire');
+		var img = document.getElementById("screenshot-img");
+		makeHighResScreenshot(src, img, 96);
+		
+		
+//		var w = window.innerWidth;	
+//		var h = window.innerHeight;
+//		var div = document.querySelector('#questionnaire');
+//		var canvas = document.createElement('canvas');
+//		canvas.width = w*2;
+//		canvas.height = h*2;
+//		canvas.style.width = w + 'px';
+//		canvas.style.height = h + 'px';
+//		var context = canvas.getContext('2d');
+//		context.scale(1,1);
+//		html2canvas(div, { canvas: canvas }).then(function(canvas) {
+//			var img=canvas.toDataURL("image/png");
+//			   var doc = new jsPDF();
+////			   var width = doc.internal.pageSize.width;    
+////			   var height = doc.internal.pageSize.height;
+//			   
+//			   doc.addImage(img,'JPEG', 0, 0, 672, 950);
+//			   doc.save('test.pdf');
+//		});
+		
+		
+		
+//		html2canvas(document.getElementById("questionnaire"),{
+//			   onrendered:function(canvas){
+//
+//			   
+//			   }
+//
+//			   });
+//		var doc = new jsPDF();
+//
+//		var specialElementHandlers = {
+//			'.controls' : function(element, renderer) {
+//				return true;
+//			}
+//		};
+//
+//		doc.fromHTML($('body').html(), 15, 15, {
+//			'width' : 170,
+//			'elementHandlers' : specialElementHandlers
+//		});
+//		doc.save('test.pdf');
+	});
+	});
+});
